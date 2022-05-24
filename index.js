24a2f6c99e7847922389910e89670323d11a6909
mosheurpau <mosheur.pau@gamil.com>
2022-05-24 20:34:44 +0600

    create user add review added api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -28,11 +28,24 @@ async function run() {
       res.send(parts);
     });
 
+    app.get("/part/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await partsCollection.findOne(query);
+      res.send(item);
+    });
+
     app.get("/review", async (req, res) => {
       const query = {};
       const cursor = reviewsCollection.find(query);
       const reviews = await cursor.toArray();
       res.send(reviews);
+    });
+
+    app.post("/review", async (req, res) => {
+      const newReview = req.body;
+      const result = await reviewsCollection.insertOne(newReview);
+      res.send(result);
     });
   } finally {
   }
