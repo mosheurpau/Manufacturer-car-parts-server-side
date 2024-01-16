e049750e6e11af581129a030fe12c27e0dd67c88
mosheurpau <mosheur.pau@gamil.com>
2024-01-16 19:34:19 +0600

    added new pass

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 59999;
 
 app.use(cors());
 app.use(express.json());
